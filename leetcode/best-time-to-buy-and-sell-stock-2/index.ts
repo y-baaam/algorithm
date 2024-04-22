d4164671790adb98bb3abae50168815378b0a9ca
young beom <dudqja3674@gmail.com>
2024-04-22 22:18:17 +0900

    best-time-to-buy-and-sell-stock-2

---
+++ leetcode/best-time-to-buy-and-sell-stock-2/index.ts
@@ -0,0 +1,10 @@
+function maxProfit2(prices: number[]): number {
+  let profit: number = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      profit += prices[i] - prices[i - 1];
+    }
+  }
+  return profit;
+}
